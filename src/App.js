ec37eb922f527dd6769546b450900bbbc8f425c8
Hamzah Fayad <hamzahfayad99@gmail.com>
2023-01-28 17:28:59 +0100

    search + results

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  const API = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+  const [word, setWord] = useState("");
+
+  const [result, setResult] = useState({});
+
+  const handleSetWord = (e) => {
+    setWord(e.target.value);
+  };
+
+  const handleSearch = () => {
+    axios.get(API + word).then((res) => {
+      setResult(res.data[0]);
+      console.log(res.data[0]);
+    });
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="sectionSize">
+        <div className="uiSection nav">
+          <img className="App-logo" src="./assets/logo.svg" alt="logo" />
+          <img
+            className="icon icon-xs"
+            src="./assets/moon.svg"
+            alt="moon_switch"
+          />
+        </div>
+        <div className="uiSection searchBar">
+          <input
+            value={word}
+            onChange={handleSetWord}
+            placeholder="Search"
+            type="text"
+          />
+          <button onClick={handleSearch}>Search</button>
+        </div>
+        <p>
+          {result?.word}, {result?.phonetics[0].text}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
